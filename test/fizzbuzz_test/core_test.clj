0d529e2a536acfa802535742d21aeb541d9e8fdc
kero_hirake <k16.h.white@gmail.com>
2021-12-23 15:58:39 +0900

    テストが失敗することを確認

--- test/fizzbuzz_test/core_test.clj
+++ test/fizzbuzz_test/core_test.clj
@@ -2,6 +2,6 @@
   (:require [clojure.test :refer :all]
             [fizzbuzz-test.core :refer :all]))
 
-(deftest a-test
+(deftest fizzbuzz-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= 0 1)) "これは失敗するはず"))
