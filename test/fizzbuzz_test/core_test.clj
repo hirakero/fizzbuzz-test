5cdde17c27e448631672689b5257d5f28ff26de9
kero_hirake <k16.h.white@gmail.com>
2022-01-06 17:02:58 +0900

    範囲のテスト

--- test/fizzbuzz_test/core_test.clj
+++ test/fizzbuzz_test/core_test.clj
@@ -16,4 +16,21 @@
     (testing "その他の数のときは、そのまま数を文字列に変換する"
       (testing "1を渡すと文字列1を返す"
         (is (= "1" (fizzbuzz 1))))))
-  )
+  (testing "範囲のテスト"
+    (testing "1から15までの数を渡すと、「1 2 Fizz 3 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz」を返す "
+      (are [x y] (= x y)
+        "1" (fizzbuzz 1)
+        "2" (fizzbuzz 2)
+        "Fizz" (fizzbuzz 3)
+        "4" (fizzbuzz 4)
+        "Buzz" (fizzbuzz 5)
+        "Fizz" (fizzbuzz 6)
+        "7" (fizzbuzz 7)
+        "8" (fizzbuzz 8)
+        "Fizz" (fizzbuzz 9)
+        "Buzz" (fizzbuzz 10)
+        "11" (fizzbuzz 11)
+        "Fizz" (fizzbuzz 12)
+        "13" (fizzbuzz 13)
+        "14" (fizzbuzz 14)
+        "FizzBuzz" (fizzbuzz 15)))))
